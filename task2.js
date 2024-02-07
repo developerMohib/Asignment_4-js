381dcb0b54ad0ea13573f662ae23242ef9b8fa5e
Mohib <mohibullahmohim2020@gmail.com>
2024-02-08 02:33:07 +0600

    sundor comment kortam

--- task2.js
+++ task2.js
@@ -56,16 +56,16 @@ document.write(myName , ' <br> ');
 
 // Task 3
 
-/*
-Problem 03 : Virus in my Array
-⚠️ Function Name Must be deleteInvalids()
-ত োমাকে deleteInvalids() নামে এমন একটা ফাংশন ক্রি য়ে ট করতে হবে যা ইনপুট হি সে বে একটি Array
-নে বে । ফাংশন টি আরে কটি Array রি টার্ন করবে যে খানে ভ্যালুগুল ো হবে শুধুনাম্বার । অর্থাৎ Array এর যে সকল
-elements নাম্বার না , তাদে র ডি লি ট করে শুধুমাত্র নাম্বার গুল ো কে Array আকারে রি টার্ন করতে হবে ।
-তুমি যদি এই ফাংশনে এই [ NaN, 1,12,0 ,-1 , undefined ] Array টি পাস কর ো কর ো তাহলে সে ত োমাকে
-রি টার্ন করবে [ 1 , 12 , 0 , -1 ]
-Input :
-ফাংশন টি ইনপুট নে বে একটি Array । Array এর উপাদান গুল োর Datatype যে ক োন ধরনে র হতে পারে ।
-Output :
-ফাংশন টি আউটপুট দে বে আরে কটি Array । Array এর উপাদান গুল ো হবে শুধুমাত্র নাম্বার ।
-*/
+
+function deleteInvalids(array) {
+    let newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (typeof (array[i]) === 'number' && !isNaN(array[i])) {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+let array = [NaN, 1, 12, 0, -1, undefined];
+let callArray = deleteInvalids(array);
+document.write(callArray)
